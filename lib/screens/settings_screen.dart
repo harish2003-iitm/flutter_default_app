22dfe0f9632566725948eb39f3569d06bcfbb2fc
G.K.Harish Balaji <95614337+harish2003-iitm@users.noreply.github.com>
2024-01-14 23:31:08 +0530

    Update settings_screen.dart

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,12 +1,14 @@
-import 'flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Settings')),
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
       body: Center(
-child: Text('Settings Screen'),
+        child: Text('Settings Screen'),
       ),
     );
   }
